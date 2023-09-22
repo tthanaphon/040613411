ed6f32cba0d8e0e8096b220c914596db72044e48
tthanaphon <s6404062630384@email.kmutnb.ac.th>
2023-09-22 17:13:49 +0700

    delete

--- lab8 DB/formget.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<form action="test.php" method="post">
-Country: <input type="text" name="country"><br>
-Language: <input type="text" name="lang"><br>
-<input type="submit">
-</form>
